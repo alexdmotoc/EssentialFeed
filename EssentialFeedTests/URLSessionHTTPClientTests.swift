add22349da0824587b6dd0f42c95c55f18a8f3f4
Alex Motoc <alexdmotoc@gmail.com>
2023-08-01 23:21:10 +0300

    ensure stubs are cleared when stop intercepting requests

--- EssentialFeedTests/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/URLSessionHTTPClientTests.swift
@@ -70,6 +70,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         static func stopInterceptingRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
+            stubs = [:]
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
