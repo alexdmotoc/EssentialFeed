10b356dbc44d7fa35ebf374768861263c476587c
Alex Motoc <alexdmotoc@gmail.com>
2023-08-01 18:04:47 +0300

    make FeedItem public

--- EssentialFeed/Feed feature/FeedItem.swift
+++ EssentialFeed/Feed feature/FeedItem.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct FeedItem {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imageURL: URL
+public struct FeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
 }
