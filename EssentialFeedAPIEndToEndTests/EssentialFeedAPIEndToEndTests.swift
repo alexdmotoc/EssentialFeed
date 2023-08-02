283ce781e7bfaa12d38c4e27f7ac444ac7126c69
Alex Motoc <alexdmotoc@gmail.com>
2023-08-02 16:12:59 +0300

    increase timeout

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -44,7 +44,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             exp.fulfill()
         }
         
-        wait(for: [exp], timeout: 5)
+        wait(for: [exp], timeout: 10)
         
         return receivedResult
     }
