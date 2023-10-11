fba00193a66bbe4ca94b0f885a151e43fa986e5c
Alex Motoc <alexdmotoc@gmail.com>
2023-10-11 16:26:33 +0300

    test load feed is performed once view is loaded

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -6,22 +6,55 @@
 //
 
 import XCTest
+import EssentialFeed
 
-class FeedViewController {
+class FeedViewController: UIViewController {
+    private var loader: FeedLoader?
     
-}
-
-class LoaderSpy {
-    var loadCount = 0
+    convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loader?.load { _ in }
+    }
 }
 
 final class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoadFeed() {
-        let sut = FeedViewController()
-        let loader = LoaderSpy()
+        let (_, loader) = makeSUT()
         
         XCTAssertEqual(loader.loadCount, 0)
+    }
+    
+    func test_loadView_loadsTheFeed() {
+        let (sut, loader) = makeSUT()
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCount, 1)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        checkIsDeallocated(sut: loader, file: file, line: line)
+        checkIsDeallocated(sut: sut, file: file, line: line)
+        return (sut, loader)
+    }
+    
+    private class LoaderSpy: FeedLoader {
+        var loadCount = 0
+        
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            loadCount += 1
+        }
     }
 
 }
