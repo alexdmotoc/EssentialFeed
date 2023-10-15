2ad840ff52e4ca73d76a554aa8ead71f94efff57
Alex Motoc <alexdmotoc@gmail.com>
2023-10-15 20:38:54 +0300

    move view models into their separate files

---
+++ EssentialFeediOS/View Models/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alex Motoc on 15.10.2023.
+//
+
+import EssentialFeed
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/View Models/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Alex Motoc on 15.10.2023.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedItem]
+}
