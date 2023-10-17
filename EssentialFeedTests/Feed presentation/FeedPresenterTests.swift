971067243657eeb8d1ae5a8b8d00fb8e33c55a85
Alex Motoc <alexdmotoc@gmail.com>
2023-10-17 18:13:52 +0300

    test did end loading sends correct messages

--- EssentialFeedTests/Feed presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed presentation/FeedPresenterTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import EssentialFeed
 
 protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
@@ -15,18 +16,29 @@ protocol FeedErrorView {
     func display(_ viewModel: FeedErrorViewModel)
 }
 
+protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
+
 final class FeedPresenter {
     private let feedLoadingView: FeedLoadingView
+    private let feedView: FeedView
     private let errorView: FeedErrorView
     
-    init(feedLoadingView: FeedLoadingView, errorView: FeedErrorView) {
+    init(feedLoadingView: FeedLoadingView, feedView: FeedView, errorView: FeedErrorView) {
         self.feedLoadingView = feedLoadingView
+        self.feedView = feedView
         self.errorView = errorView
     }
     
     func didStartLoading() {
         errorView.display(.noError)
         feedLoadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+    
+    func didEndLoading(with feed: [FeedItem]) {
+        feedLoadingView.display(FeedLoadingViewModel(isLoading: false))
+        feedView.display(FeedViewModel(feed: feed))
     }
 }
 
@@ -46,21 +58,31 @@ final class FeedPresenterTests: XCTestCase {
         XCTAssertEqual(spy.messages, [.display(error: nil), .display(isLoading: true)])
     }
     
+    func test_endLoading_sendsCorrectMessages() {
+        let (sut, spy) = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        sut.didEndLoading(with: feed)
+        
+        XCTAssertEqual(spy.messages, [.display(isLoading: false), .display(feed: feed)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let spy = ViewSpy()
-        let sut = FeedPresenter(feedLoadingView: spy, errorView: spy)
+        let sut = FeedPresenter(feedLoadingView: spy, feedView: spy, errorView: spy)
         checkIsDeallocated(sut: spy, file: file, line: line)
         checkIsDeallocated(sut: sut, file: file, line: line)
         return (sut, spy)
     }
     
-    private class ViewSpy: FeedLoadingView, FeedErrorView {
+    private class ViewSpy: FeedLoadingView, FeedErrorView, FeedView {
         
         enum Message: Equatable {
             case display(error: String?)
             case display(isLoading: Bool)
+            case display(feed: [FeedItem])
         }
         
         private(set) var messages: [Message] = []
@@ -71,6 +93,10 @@ final class FeedPresenterTests: XCTestCase {
         
         func display(_ viewModel: FeedErrorViewModel) {
             messages.append(.display(error: viewModel.message))
+        }
+        
+        func display(_ viewModel: FeedViewModel) {
+            messages.append(.display(feed: viewModel.feed))
         }
     }
 }
