ce407124ca6c8e52917a98335e51a7e630ea4f6a
Alex Motoc <alexdmotoc@gmail.com>
2023-10-18 18:33:13 +0300

    separate CoreDataFeedStore FeedStore implementation into own file

--- EssentialFeed/Feed cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed cache/LocalFeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class LocalFeedLoader {
+public final class LocalFeedLoader: FeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
     
