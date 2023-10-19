fe6b15f451f74b5c18ddd0987be7e9958d2ef6ca
Alex Motoc <alexdmotoc@gmail.com>
2023-10-19 20:04:30 +0300

    `FeedLoaderCacheDecorator` load delivers feed on success

---
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -0,0 +1,45 @@
+//
+//  FeedLoaderCacheDecoratorTests.swift
+//  EssentialAppTests
+//
+//  Created by Alex Motoc on 19.10.2023.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    
+    init(decoratee: FeedLoader) {
+        self.decoratee = decoratee
+    }
+    
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load(completion: completion)
+    }
+}
+
+final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTests {
+    
+    func test_load_deliversFeedOnSuccess() {
+        let feed = uniqueImageFeed().models
+        let sut = makeSUT(stub: .success(feed))
+        
+        expect(sut, toCompleteWith: .success(feed))
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        stub: FeedLoader.Result,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> FeedLoaderCacheDecorator {
+        let loader = FeedLoaderStub(stub: stub)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        checkIsDeallocated(sut: loader, file: file, line: line)
+        checkIsDeallocated(sut: sut, file: file, line: line)
+        return sut
+    }
+}
