ed4a9d2158da5b81389ce0da700a3c2cb5affc64
Alex Motoc <alexdmotoc@gmail.com>
2023-10-19 20:15:15 +0300

    `FeedLoaderCacheDecorator` caches feed on load success

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -8,15 +8,24 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedCache {
+    func save(_ feed: [FeedItem], completion: @escaping (Error?) -> Void)
+}
+
 final class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
+    private let cache: FeedCache
     
-    init(decoratee: FeedLoader) {
+    init(decoratee: FeedLoader, cache: FeedCache) {
         self.decoratee = decoratee
+        self.cache = cache
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load(completion: completion)
+        decoratee.load { [weak self] result in
+            self?.cache.save((try? result.get()) ?? []) { _ in }
+            completion(result)
+        }
     }
 }
 
@@ -35,17 +44,41 @@ final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTests {
         expect(sut, toCompleteWith: .failure(anyNSError()))
     }
     
+    func test_cache_cachesFeedOnLoadSuccess() {
+        let feed = uniqueImageFeed().models
+        let cache = CacheSpy()
+        let sut = makeSUT(stub: .success(feed), cache: cache)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(cache.messages, [.save(feed: feed)])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(
         stub: FeedLoader.Result,
+        cache: CacheSpy = .init(),
         file: StaticString = #filePath,
         line: UInt = #line
     ) -> FeedLoaderCacheDecorator {
         let loader = FeedLoaderStub(stub: stub)
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader, cache: cache)
+        checkIsDeallocated(sut: cache, file: file, line: line)
         checkIsDeallocated(sut: loader, file: file, line: line)
         checkIsDeallocated(sut: sut, file: file, line: line)
         return sut
+    }
+    
+    private class CacheSpy: FeedCache {
+        enum Message: Equatable {
+            case save(feed: [FeedItem])
+        }
+        
+        private(set) var messages: [Message] = []
+        
+        func save(_ feed: [FeedItem], completion: @escaping (Error?) -> Void) {
+            messages.append(.save(feed: feed))
+        }
     }
 }
