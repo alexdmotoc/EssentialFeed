1c401503e100cca807e707023de40674295b7307
Alex Motoc <alexdmotoc@gmail.com>
2023-10-20 17:13:09 +0300

    add configuration for UI tests, integrated into CI as well

---
+++ EssentialApp/EssentialAppAcceptanceUITests/EssentialAppAcceptanceUITests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppAcceptanceUITests.swift
+//  EssentialAppAcceptanceUITests
+//
+//  Created by Alex Motoc on 20.10.2023.
+//
+
+import XCTest
+
+final class EssentialAppAcceptanceUITests: XCTestCase {
+
+    
+}
