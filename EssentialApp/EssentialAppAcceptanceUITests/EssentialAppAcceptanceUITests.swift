2d6f167d46a6ea67f8e09a7b0122b77f2e518c21
Alex Motoc <alexdmotoc@gmail.com>
2023-10-20 17:36:35 +0300

    reset app on all tests to ensure tests are isolated from side effects

--- EssentialApp/EssentialAppAcceptanceUITests/EssentialAppAcceptanceUITests.swift
+++ EssentialApp/EssentialAppAcceptanceUITests/EssentialAppAcceptanceUITests.swift
@@ -11,6 +11,7 @@ final class EssentialAppAcceptanceUITests: XCTestCase {
 
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
+        app.launchArguments = ["-reset"]
         app.launch()
         
         let feedCells = app.cells.matching(identifier: "feed-image-cell")
@@ -22,6 +23,7 @@ final class EssentialAppAcceptanceUITests: XCTestCase {
     
     func test_onLaunch_displaysCachedFeedWhenThereIsNoConnectivity() {
         let onlineApp = XCUIApplication()
+        onlineApp.launchArguments = ["-reset"]
         onlineApp.launch()
         
         let offlineApp = XCUIApplication()
