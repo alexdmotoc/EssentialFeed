0399a72efcbb4907a38919920b56c8092a30b6b8
Alex Motoc <alexdmotoc@gmail.com>
2023-10-26 15:35:16 +0300

    separate shared API from feed API

--- EssentialFeedTests/Helpers/XCTestCase+Utils.swift
+++ EssentialFeedTests/Helpers/XCTestCase+Utils.swift
@@ -18,30 +18,30 @@ extension XCTestCase {
             XCTAssertNil(sut, file: file, line: line)
         }
     }
-    
-    func uniqueImage() -> FeedItem {
-        FeedItem(id: UUID(), description: "any", location: "any", imageURL: URL(string: "some-url.com")!)
-    }
-    
-    func uniqueImageFeed() -> (local: [LocalFeedImage], models: [FeedItem]) {
-        let items = [
-            uniqueImage(),
-            uniqueImage(),
-            uniqueImage()
-        ]
-        let local = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.imageURL) }
-        return (local, items)
-    }
-    
-    func anyNSError() -> NSError {
-        NSError(domain: "com.tests.mockError", code: 0)
-    }
-    
-    func anyURL() -> URL {
-        .init(string: "https://some-url.com")!
-    }
-    
-    func anyData() -> Data {
-        Data("any data".utf8)
-    }
+}
+
+func uniqueImage() -> FeedItem {
+    FeedItem(id: UUID(), description: "any", location: "any", imageURL: URL(string: "some-url.com")!)
+}
+
+func uniqueImageFeed() -> (local: [LocalFeedImage], models: [FeedItem]) {
+    let items = [
+        uniqueImage(),
+        uniqueImage(),
+        uniqueImage()
+    ]
+    let local = items.map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.imageURL) }
+    return (local, items)
+}
+
+func anyNSError() -> NSError {
+    NSError(domain: "com.tests.mockError", code: 0)
+}
+
+func anyURL() -> URL {
+    .init(string: "https://some-url.com")!
+}
+
+func anyData() -> Data {
+    Data("any data".utf8)
 }
