7b5287613e459f98dfa98543b56bd6275c3b6bac
Alex Motoc <alexdmotoc@gmail.com>
2023-11-01 19:17:16 +0200

    remove deprecated FeedImageDataStore methods

--- EssentialFeed/Feed cache/FeedImageDataStore.swift
+++ EssentialFeed/Feed cache/FeedImageDataStore.swift
@@ -8,44 +8,6 @@
 import Foundation
 
 public protocol FeedImageDataStore {
-    typealias RetrievalResult = Swift.Result<Data?, Error>
-    typealias InsertionResult = Swift.Result<Void, Error>
-    
-    @available(*, deprecated)
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void)
-    
-    @available(*, deprecated)
-    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void)
-    
     func insert(_ data: Data, for url: URL) throws
     func retrieve(dataForURL url: URL) throws -> Data?
-}
-
-public extension FeedImageDataStore {
-    func insert(_ data: Data, for url: URL) throws {
-        let group = DispatchGroup()
-        var receivedResult: InsertionResult!
-        group.enter()
-        insert(data, for: url) { result in
-            receivedResult = result
-            group.leave()
-        }
-        group.wait()
-        return try receivedResult.get()
-    }
-    
-    func retrieve(dataForURL url: URL) throws -> Data? {
-        let group = DispatchGroup()
-        var receivedResult: RetrievalResult!
-        group.enter()
-        retrieve(dataForURL: url) { result in
-            receivedResult = result
-            group.leave()
-        }
-        group.wait()
-        return try receivedResult.get()
-    }
-    
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {}
-    func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {}
 }
